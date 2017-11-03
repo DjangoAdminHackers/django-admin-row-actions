d521ede7a2594373e5803e45918950837e655124
Drew Hubl <drew.hubl@gmail.com>
2017-11-02 22:35:44 -0700

    Bump version to 0.0.4 for new release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-admin-row-actions',
-    version='0.0.3',
+    version='0.0.4',
     description='django admin row actions',
     author='Andy Baker',
     author_email='andy@andybak.net',
