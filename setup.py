9d6c1720aee11c6699ed320a8bae6d75121393f7
Chen Zhe <fruitschen@gmail.com>
2019-05-31 00:48:47 +0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-admin-row-actions',
-    version='0.0.4',
+    version='0.0.5',
     description='django admin row actions',
     author='Andy Baker',
     author_email='andy@andybak.net',
